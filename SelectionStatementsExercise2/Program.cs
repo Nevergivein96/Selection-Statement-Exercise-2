63336b39e1b7097995f3053e70eb577abcf1734b
Spencer Hansen <spencerhansen@gmail.com>
2022-12-15 18:08:09 -0700

    exercise 2 completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("please type your favorite subject in school");
+            var userInput=Console.ReadLine();
+
+            switch(userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("math is my favorite as well)) i love how its not subjective!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("science is pretty cool. seems like people do a lot of stuff with it");
+                    break;
+
+                case "history":
+                    Console.WriteLine("history is very interesting! i do wonder though if it ought to be pursued as a career");
+                    break;
+
+                case "writing":
+                    Console.WriteLine("this is a great subject! the power of the written word is incredible");
+                    break;
+
+                case "physical education":
+                case "pe":
+                    Console.WriteLine("it is very important to stay healthy and strong! good choice");
+                    break;
+
+                default: Console.WriteLine("That sounds interesting i will have to look into that");
+                    break;
+            }
         }
     }
 }
